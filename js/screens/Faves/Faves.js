003b5f6a2dcd9102b3f91fd50d5887100da5c593
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-03 12:11:32 -0700

    Add placeholder message when no Faves selected

--- js/screens/Faves/Faves.js
+++ js/screens/Faves/Faves.js
@@ -1,9 +1,14 @@
 import React from "react";
 import SessionList from "../../components/SessionList";
+import { Text } from "react-native";
 import PropTypes from "prop-types";
 
 const Faves = ({ faveIds, sessionsData }) => {
-  return <SessionList faveIds={faveIds} sessionsData={sessionsData} />;
+  return faveIds.length < 1 ? (
+    <Text>No faves yet</Text>
+  ) : (
+    <SessionList faveIds={faveIds} sessionsData={sessionsData} />
+  );
 };
 
 Faves.propTypes = {
